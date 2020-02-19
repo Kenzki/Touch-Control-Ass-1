6bdcf84058360ce6f77cd74a67db613dfab5a391
Kenneth Malon <Kenneth.Malon@students.ittralee.ie>
2020-02-19 18:14:52 +0000

    Worked on Camera Scaling

--- Touch Control Assignment/Assets/TouchControl.cs
+++ Touch Control Assignment/Assets/TouchControl.cs
@@ -18,6 +18,8 @@ public class TouchControl : MonoBehaviour
     private Quaternion initial_object_orientation;
     private float initial_distance;
     private Vector3 initial_scale;
+    private float previousDistance;
+    private float scaleSpeed = 3f;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +32,7 @@ public class TouchControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        cameraScaling();
 
         if (Input.touchCount == 2)
         {
@@ -131,4 +134,25 @@ public class TouchControl : MonoBehaviour
 
     }
 
+    void cameraScaling()
+    {
+        if(Input.touchCount == 2 && (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(1).phase == TouchPhase.Began))
+        {
+            previousDistance = Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
+
+        }
+
+        else if(Input.touchCount == 2 && (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(1).phase == TouchPhase.Moved))
+        {
+            float distance;
+            Vector2 touch1 = Input.GetTouch(0).position;
+            Vector2 touch2 = Input.GetTouch(1).position;
+
+            distance = Vector2.Distance(touch1, touch2);
+            float pinchAmount = (previousDistance - distance) * scaleSpeed * Time.deltaTime;
+            my_camera.transform.Translate(0, 0, pinchAmount);
+
+            previousDistance = distance;
+        }
+    }
 }
